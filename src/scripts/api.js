f38164845cffc27efa4ea9b1c5593d2003b38219
Akhee123 <mabuirmelah@gmail.com>
2023-02-25 17:09:50 +0200

    API Handling

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,37 +1,36 @@
+const testURL = "https://63f9319e473885d837cafdd9.mockapi.io/shoes/";
 
-const testURL = 'https://63f9319e473885d837cafdd9.mockapi.io/shoes/';
+export async function getData(url) {
+  const reponse = await fetch(url);
+  const data = await reponse.json();
 
-export async function getData(url){
-    const reponse = await fetch(url);
-    const data = await reponse.json();
-
-    return data;
+  return data;
 }
 
-export async function addData(url, data){
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    });
+export async function addData(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
 
-    return response;
+  return response;
 }
 
-export async function updateData(url, index, data){
-    const response = await fetch((url + index), {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    });
+export async function updateData(url, index, data) {
+  const response = await fetch(url + index, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
 
-    return response;
+  return response;
 }
 
-export async function deleteData(url, index){
-    const response = await fetch(url, {
-        method: 'DELETE',
-    });
+export async function deleteData(url, index) {
+  const response = await fetch(url, {
+    method: "DELETE",
+  });
 
-    return response;
+  return response;
 }
